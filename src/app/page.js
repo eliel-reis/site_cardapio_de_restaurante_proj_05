ce72d7575835e928205e33a63f84a5a82232b7b3
Eliel Reis <elielreis651@gmail.com>
2024-07-22 14:24:02 -0300

    esse é o build para conseguir fazer o deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,43 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+
+
 import styles from "./page.module.css";
+import Topo from "@/components/Topo";
+import Categoria from "@/components/Categoria";
+import Campo_De_Busca from "@/components/Campo_De_Busca";
+import Cards from "@/components/Cards";
+
+
+
 
 export default function Home() {
+  const [btn_clicado, set_btn_clicado] = useState(0);
+
+  const [tipo_categoria, set_tipo_categoria] = useState("Entradas");
+
+  const [novo_valor, set_novo_valor] = useState("")
+
+
+
+
+  const onChang = (texto_digitado) => {
+    ((texto_digitado.toString().length >= 3) &&
+      set_tipo_categoria(texto_digitado));
+    set_novo_valor(texto_digitado);
+    set_btn_clicado(0)
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Topo />
+      <section className={styles.container_principal} >
+        <Categoria valor_digitado={set_novo_valor} resultado_btn={btn_clicado} estado_btn={set_btn_clicado} type_categoria={set_tipo_categoria} />
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <Campo_De_Busca valor_digitado={novo_valor} busca={onChang} />
+        <Cards type_categoria={tipo_categoria.toString()} />
+      </section>
     </main>
   );
 }
